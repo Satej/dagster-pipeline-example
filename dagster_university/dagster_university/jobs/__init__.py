834cab323088c7001fa08442e7c288cd39ab1414
Satej <Satej@users.noreply.github.com>
2024-08-07 18:55:34 +0000

    Added partitions and backfills

--- dagster_university/dagster_university/jobs/__init__.py
+++ dagster_university/dagster_university/jobs/__init__.py
@@ -1,14 +1,21 @@
-from dagster import AssetSelection
-from dagster import AssetSelection, define_asset_job
+from dagster import define_asset_job, AssetSelection, AssetKey
+from ..partitions import monthly_partition, weekly_partition
 
 trips_by_week = AssetSelection.assets("trips_by_week")
 
 trip_update_job = define_asset_job(
     name="trip_update_job",
-    selection=AssetSelection.all() - trips_by_week
+    partitions_def=monthly_partition, # partitions added here
+    selection=AssetSelection.all() - AssetSelection.assets(["trips_by_week"])
 )
 
 weekly_update_job = define_asset_job(
     name="weekly_update_job",
+    selection=trips_by_week,
+)
+
+weekly_update_job = define_asset_job(
+    name="weekly_update_job",
+    partitions_def=weekly_partition,
     selection=trips_by_week,
 )
