237c3808ae84482a7a5a40fd54cf2083f0355205
Satej <Satej@users.noreply.github.com>
2024-08-07 22:11:22 +0530

    Update trips.py

--- dagster_university/dagster_university/assets/trips.py
+++ dagster_university/dagster_university/assets/trips.py
@@ -1,6 +1,7 @@
 import requests
 from . import constants
 from dagster import asset
+from dagster_duckdb import DuckDBResource
 import duckdb
 import os
 
@@ -33,7 +34,7 @@ def taxi_zones_file() -> None:
 @asset(
     deps=["taxi_trips_file"]
 )
-def taxi_trips() -> None:
+def taxi_trips(database: DuckDBResource) -> None:
     """
       The raw taxi trips dataset, loaded into a DuckDB database
     """
@@ -54,13 +55,13 @@ def taxi_trips() -> None:
         );
     """
 
-    conn = duckdb.connect(os.getenv("DUCKDB_DATABASE"))
-    conn.execute(sql_query)
+    with database.get_connection() as conn:
+        conn.execute(sql_query)
 
 @asset(
     deps=["taxi_zones_file"]
 )
-def taxi_zones() -> None:
+def taxi_zones(database: DuckDBResource) -> None:
     sql_query = f"""
         create or replace table zones as (
             select
@@ -72,5 +73,5 @@ def taxi_zones() -> None:
         );
     """
 
-    conn = duckdb.connect(os.getenv("DUCKDB_DATABASE"))
-    conn.execute(sql_query)
+    with database.get_connection() as conn:
+        conn.execute(sql_query)
