09d22d7531b9f207d68efa3a55a1e37816e3abf6
Satej <Satej@users.noreply.github.com>
2024-08-07 22:08:51 +0530

    Update metrics.py

--- dagster_university/dagster_university/assets/metrics.py
+++ dagster_university/dagster_university/assets/metrics.py
@@ -2,6 +2,7 @@ from dagster import asset
 
 from . import constants
 from dagster import asset
+from dagster_duckdb import DuckDBResource
 
 import plotly.express as px
 import plotly.io as pio
@@ -17,7 +18,7 @@ from . import constants
 @asset(
     deps=["taxi_trips", "taxi_zones"]
 )
-def manhattan_stats() -> None:
+def manhattan_stats(database: DuckDBResource) -> None:
     query = """
         select
             zones.zone,
@@ -30,8 +31,8 @@ def manhattan_stats() -> None:
         group by zone, borough, geometry
     """
 
-    conn = duckdb.connect(os.getenv("DUCKDB_DATABASE"))
-    trips_by_zone = conn.execute(query).fetch_df()
+    with database.get_connection() as conn:
+        trips_by_zone = conn.execute(query).fetch_df()
 
     trips_by_zone["geometry"] = gpd.GeoSeries.from_wkt(trips_by_zone["geometry"])
     trips_by_zone = gpd.GeoDataFrame(trips_by_zone)
@@ -62,9 +63,7 @@ def manhattan_map() -> None:
 @asset(
     deps=["taxi_trips"]
 )
-def trips_by_week() -> None:
-    conn = duckdb.connect(os.getenv("DUCKDB_DATABASE"))
-
+def trips_by_week(database: DuckDBResource) -> None:
     current_date = datetime.strptime("2023-03-01", constants.DATE_FORMAT)
     end_date = datetime.strptime("2023-04-01", constants.DATE_FORMAT)
 
@@ -79,7 +78,8 @@ def trips_by_week() -> None:
             where date_trunc('week', pickup_datetime) = date_trunc('week', '{current_date_str}'::date)
         """
 
-        data_for_week = conn.execute(query).fetch_df()
+        with database.get_connection() as conn:
+            data_for_week = conn.execute(query).fetch_df()
 
         aggregate = data_for_week.agg({
             "vendor_id": "count",
